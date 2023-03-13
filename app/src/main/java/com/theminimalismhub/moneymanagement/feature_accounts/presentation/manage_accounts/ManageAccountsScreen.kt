ea22252dea5b9bd42191ba5f37a07dbc28fa38f4
StraleXY <strahinja0123@gmail.com>
2023-03-13 21:49:04 +0100

    Refactor: Added sliding content composable

--- app/src/main/java/com/theminimalismhub/moneymanagement/feature_accounts/presentation/manage_accounts/ManageAccountsScreen.kt
+++ app/src/main/java/com/theminimalismhub/moneymanagement/feature_accounts/presentation/manage_accounts/ManageAccountsScreen.kt
@@ -3,7 +3,6 @@ package com.theminimalismhub.moneymanagement.feature_accounts.presentation.manag
 import android.util.Log
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,11 +14,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
@@ -31,7 +27,6 @@ import com.google.accompanist.pager.*
 import com.ramcosta.composedestinations.annotation.Destination
 import com.theminimalismhub.moneymanagement.core.composables.ScreenHeader
 import com.theminimalismhub.moneymanagement.core.transitions.BaseTransition
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -41,7 +36,6 @@ fun ManageAccountsScreen(
 ) {
 
     val state = vm.state.value
-    val scope = rememberCoroutineScope()
 
     val pagerState = rememberPagerState(
         pageCount = state.accounts.size,
@@ -54,19 +48,6 @@ fun ManageAccountsScreen(
     var accountsPagerHeight by remember { mutableStateOf(0.dp) }
     val screenHeight by remember { mutableStateOf(Dp(root.height / density)) }
     val scroll: ScrollState = rememberScrollState(0)
-
-    LaunchedEffect(scroll.isScrollInProgress && scroll.value.dp > 5.dp && scroll.value < (headerHeight - accountsPagerHeight).value ) {
-        Log.d("SCROLL", scroll.isScrollInProgress.toString())
-        if(!scroll.isScrollInProgress) {
-            if (scroll.value.dp < 150.dp) scroll.animateScrollTo(0, tween(175))
-            else if (scroll.value < (headerHeight - accountsPagerHeight + 64.dp).value) scroll.animateScrollTo((headerHeight - accountsPagerHeight + 65.dp).value.toInt(), tween(175))
-        }
-    }
-
-    val TAG = "ACCOUNT"
-    LaunchedEffect(screenHeight) { Log.w(TAG, "Screen Height $screenHeight | Density $density")}
-    LaunchedEffect(headerHeight) { Log.w(TAG, "Header Height $headerHeight")}
-    LaunchedEffect(accountsPagerHeight) { Log.w(TAG, "Cards Pager Height $accountsPagerHeight")}
 
 
     Box(modifier = Modifier.fillMaxSize()) {
@@ -84,7 +65,7 @@ fun ManageAccountsScreen(
                         .graphicsLayer {
                             translationY = -(scroll.value.toFloat() * 1.2f)
                                 .coerceAtLeast(0f)
-                                .coerceAtMost(440f)
+                                .coerceAtMost(430f)
                         }
                 ) {
                     ScreenHeader(
@@ -106,63 +87,80 @@ fun ManageAccountsScreen(
             }
 
         }
-
-        var textTrans by remember { mutableStateOf(0f) }
-        Column(modifier = Modifier.fillMaxSize()) {
-            Spacer(Modifier.height(accountsPagerHeight))
-            // CARD
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(15.dp))
-                    .graphicsLayer {
-                        translationY =
-                            ((headerHeight - accountsPagerHeight - scroll.value.dp).toPx()).coerceAtLeast(0f)
-                    }
-                    .verticalScroll(scroll)
-            ) {
-                // CONTENT CONTAINER - translates content to the top and then keeps it there
-                Column(
+        SlidingCard(
+            headerHeight = headerHeight,
+            accountsPagerHeight = accountsPagerHeight,
+            screenHeight = screenHeight,
+            scrollState = scroll
+        ) {
+            repeat(8) {
+                Text(
+                    text = "In the modern design world, Lorem Ipsum is the industry standard when placing dummy text onto an unfinished page, whether it's a newspaper, magazine, or advertisement. The Latin text was first used in the 16th century, when a printer scrambled a row of type (known as a \"galley\") so it could be used in a book that showcased the type's quality. This text saw a resurgence when electronic typesetting became popular in the 1960s, mainly because the French typography company Letraset started selling sheets with Lorem Ipsum.",
+                    style = MaterialTheme.typography.body1,
+                    textAlign = TextAlign.Justify,
                     modifier = Modifier
-                        .fillMaxSize()
-                        .clip(RoundedCornerShape(15.dp))
-                        .graphicsLayer {
-                            val cardTranslationY = ((headerHeight - accountsPagerHeight - scroll.value.dp).toPx()).coerceAtLeast(0f)
-                            textTrans = if (cardTranslationY > 0) scroll.value.dp.toPx() / 2 else textTrans
-                            translationY = textTrans
-                        }
-                ) {
-                    // CONTENT - adds bottom podding
-                    Column(
-                        modifier = Modifier
-                            .clip(RoundedCornerShape(15.dp))
-                            .shadow(8.dp)
-                            .heightIn(screenHeight - accountsPagerHeight + 160.dp)
-                            .background(Color(0XFF212121))
-                    ) {
-                        repeat(8) {
-                            Text(
-                                text = "In the modern design world, Lorem Ipsum is the industry standard when placing dummy text onto an unfinished page, whether it's a newspaper, magazine, or advertisement. The Latin text was first used in the 16th century, when a printer scrambled a row of type (known as a \"galley\") so it could be used in a book that showcased the type's quality. This text saw a resurgence when electronic typesetting became popular in the 1960s, mainly because the French typography company Letraset started selling sheets with Lorem Ipsum.",
-                                style = MaterialTheme.typography.body1,
-                                textAlign = TextAlign.Justify,
-                                modifier = Modifier
-                                    .padding(16.dp)
-                            )
-                        }
-                        Text(text = "LRAJK")
-                        Spacer(modifier = Modifier.height(160.dp))
-                    }
-                }
+                        .padding(16.dp)
+                )
             }
+            Text(text = "LRAJK")
         }
+
     }
 }
 
 @Composable
 fun SlidingCard(
-
+    headerHeight: Dp,
+    accountsPagerHeight: Dp,
+    screenHeight: Dp,
+    scrollState: ScrollState,
+    content: @Composable ColumnScope.() -> Unit
 ) {
 
+    LaunchedEffect(scrollState.isScrollInProgress && scrollState.value.dp > 5.dp && scrollState.value < (headerHeight - accountsPagerHeight).value ) {
+        if (scrollState.value.dp < 150.dp) scrollState.animateScrollTo(0, tween(175))
+        else if (scrollState.value < (headerHeight - accountsPagerHeight + 64.dp).value) scrollState.animateScrollTo((headerHeight - accountsPagerHeight + 65.dp).value.toInt(), tween(175))
+    }
+
+    var textTrans by remember { mutableStateOf(0f) }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Spacer(Modifier.height(accountsPagerHeight))
+        // CARD
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(15.dp))
+                .graphicsLayer {
+                    translationY =
+                        ((headerHeight - accountsPagerHeight - scrollState.value.dp).toPx()).coerceAtLeast(0f)
+                }
+                .verticalScroll(scrollState)
+        ) {
+            // CONTENT CONTAINER - translates content to the top and then keeps it there
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(RoundedCornerShape(15.dp))
+                    .graphicsLayer {
+                        val cardTranslationY = ((headerHeight - accountsPagerHeight - scrollState.value.dp).toPx()).coerceAtLeast(0f)
+                        textTrans = if (cardTranslationY > 0) scrollState.value.dp.toPx() / 2 else textTrans
+                        translationY = textTrans
+                    }
+            ) {
+                // CONTENT - adds bottom podding
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(15.dp))
+                        .shadow(8.dp)
+                        .heightIn(screenHeight - accountsPagerHeight + 160.dp)
+                        .background(Color(0XFF212121))
+                ) {
+                    content()
+                    Spacer(modifier = Modifier.height(160.dp))
+                }
+            }
+        }
+    }
 }
 
