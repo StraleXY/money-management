c45547584665251fe87b04d401b2647a04c2e4dd
StraleXY <strahinja0123@gmail.com>
2023-03-13 19:25:55 +0100

    Refactor: Accounts screen

---
+++ app/src/main/java/com/theminimalismhub/moneymanagement/feature_accounts/presentation/manage_accounts/AccountComponents.kt
@@ -0,0 +1,217 @@
+package com.theminimalismhub.moneymanagement.feature_accounts.presentation.manage_accounts
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.android.material.math.MathUtils
+import com.theminimalismhub.moneymanagement.feature_accounts.domain.model.Account
+import com.theminimalismhub.moneymanagement.feature_accounts.presentation.composables.AccountCardLarge
+import kotlin.math.absoluteValue
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun AccountsPager(
+    accounts: List<Account>,
+    pagerState: PagerState,
+    onAccountSelected: (Int) -> Unit
+) {
+
+    LaunchedEffect(pagerState.currentPage) {
+        onAccountSelected(pagerState.currentPage)
+    }
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 40.dp, bottom = 40.dp)
+    ) {itemIdx ->
+        if(accounts.isEmpty()) return@HorizontalPager
+        Box(
+            modifier = Modifier
+                .graphicsLayer {
+                    val pageOffset =
+                        calculateCurrentOffsetForPage(itemIdx).absoluteValue
+
+                    MathUtils.lerp(
+                        0.95f, 1.15f, 1f - pageOffset.coerceIn(0f, 1f)
+                    ).also { scale ->
+                        scaleX = scale
+                        scaleY = scale
+                    }
+                }
+                .padding(horizontal = 12.dp)
+        ) {
+            AccountCardLarge(
+                account = accounts[itemIdx],
+                scale = 1.05f,
+                overlayStrength = 0.1f
+            )
+        }
+    }
+    Spacer(modifier = Modifier.height(8.dp))
+}
+
+@Composable
+fun AccountActions(
+    enabled: Boolean = true,
+    account: Account?,
+    onToggleActivate: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        CircularActionButton(
+            icon = Icons.Default.SyncAlt,
+            action = "Transfer",
+            enabled = enabled
+        ) { }
+        CircularActionButton(
+            modifier = Modifier
+                .alpha(animateFloatAsState(targetValue = if(enabled && !(account?.primary ?: true)) 1f else 0.65f, tween(200)).value),
+            icon = Icons.Default.CreditScore,
+            action = "Set As Primary",
+            enabled = enabled && !(account?.primary ?: true)
+        ) { }
+        CircularActionButton(
+            icon = if(account?.active != false) Icons.Default.RemoveShoppingCart else Icons.Default.AddShoppingCart,
+            action = if(account?.active != false) "Disable" else "Enable",
+            enabled = enabled,
+            onClick = onToggleActivate
+        )
+        DashedCircularActionButton(
+            icon = Icons.Default.AddCard,
+            action = "Add Account",
+            enabled = enabled
+        ) { }
+    }
+}
+
+@Composable
+fun CircularActionButton(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    action: String,
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .background(
+                    MaterialTheme.colors.surface,
+                    RoundedCornerShape(100)
+                )
+                .clip(RoundedCornerShape(100))
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = if(enabled) LocalIndication.current else null
+                ) { if(enabled) onClick() },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = icon.name
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = action,
+            style = MaterialTheme.typography.button.copy(
+                fontWeight = FontWeight.Normal,
+                letterSpacing = 0.65.sp,
+                fontSize = 14.sp
+            ),
+            color = MaterialTheme.colors.onBackground
+        )
+    }
+}
+
+@Composable
+fun DashedCircularActionButton(
+    modifier: Modifier = Modifier,
+    strokeColor: Color = MaterialTheme.colors.secondary,
+    icon: ImageVector,
+    action: String,
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    val stroke = Stroke(
+        width = 2.5f,
+        pathEffect = PathEffect.dashPathEffect(floatArrayOf(12f, 12f), 0.1f)
+    )
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(70.dp)
+                .drawBehind {
+                    drawRoundRect(
+                        color = strokeColor,
+                        style = stroke,
+                        cornerRadius = CornerRadius(40.dp.toPx())
+                    )
+                }
+                .clip(RoundedCornerShape(100))
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = if(enabled) LocalIndication.current else null
+                ) { if(enabled) onClick() },
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = icon.name
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            text = action,
+            style = MaterialTheme.typography.button.copy(
+                fontWeight = FontWeight.Normal,
+                letterSpacing = 0.65.sp,
+                fontSize = 14.sp
+            ),
+            color = MaterialTheme.colors.onBackground
+        )
+    }
+}
--- app/src/main/java/com/theminimalismhub/moneymanagement/feature_accounts/presentation/manage_accounts/ManageAccountsScreen.kt
+++ app/src/main/java/com/theminimalismhub/moneymanagement/feature_accounts/presentation/manage_accounts/ManageAccountsScreen.kt
@@ -1,53 +1,17 @@
 package com.theminimalismhub.moneymanagement.feature_accounts.presentation.manage_accounts
 
-import android.util.Log
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.LocalIndication
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.ColorUtils
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.calculateCurrentOffsetForPage
-import com.google.accompanist.pager.rememberPagerState
-import com.google.android.material.math.MathUtils.lerp
+import com.google.accompanist.pager.*
 import com.ramcosta.composedestinations.annotation.Destination
 import com.theminimalismhub.moneymanagement.core.composables.ScreenHeader
 import com.theminimalismhub.moneymanagement.core.transitions.BaseTransition
-import com.theminimalismhub.moneymanagement.feature_accounts.presentation.composables.AccountCardLarge
-import com.theminimalismhub.moneymanagement.feature_accounts.presentation.composables.AddNewAccount
-import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalPagerApi::class)
 @Composable
@@ -63,10 +27,6 @@ fun ManageAccountsScreen(
         initialOffscreenLimit = 1,
     )
 
-    LaunchedEffect(pagerState.currentPage) {
-        vm.onEvent(ManageAccountsEvent.CardSelected(pagerState.currentPage))
-    }
-
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
             contentPadding = PaddingValues(bottom = 20.dp),
@@ -78,161 +38,18 @@ fun ManageAccountsScreen(
                     title = "Management Accounts",
                     hint = "Track your balance across multiple accounts!"
                 )
-                HorizontalPager(
-                    state = pagerState,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 40.dp, bottom = 40.dp)
-                ) {itemIdx ->
-                    if(state.accounts.isEmpty()) return@HorizontalPager
-                    Box(
-                        modifier = Modifier
-                            .graphicsLayer {
-                                val pageOffset =
-                                    calculateCurrentOffsetForPage(itemIdx).absoluteValue
-
-                                lerp(
-                                    0.95f, 1.15f, 1f - pageOffset.coerceIn(0f, 1f)
-                                ).also { scale ->
-                                    scaleX = scale
-                                    scaleY = scale
-                                }
-                            }
-                            .padding(horizontal = 12.dp)
-                    ) {
-                        AccountCardLarge(
-                            account = state.accounts[itemIdx],
-                            scale = 1.05f,
-                            overlayStrength = 0.1f
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    CircularActionButton(
-                        icon = Icons.Default.SyncAlt,
-                        action = "Transfer",
-                        enabled = !pagerState.isScrollInProgress
-                    ) { }
-                    CircularActionButton(
-                        modifier = Modifier
-                            .alpha(animateFloatAsState(targetValue = if(!pagerState.isScrollInProgress && !(state.selectedAccount?.primary ?: true)) 1f else 0.65f, tween(200)).value),
-                        icon = Icons.Default.CreditScore,
-                        action = "Set As Primary",
-                        enabled = !pagerState.isScrollInProgress && !(state.selectedAccount?.primary ?: true)
-                    ) { }
-                    CircularActionButton(
-                        icon = if(state.selectedAccount?.active != false) Icons.Default.RemoveShoppingCart else Icons.Default.AddShoppingCart,
-                        action = if(state.selectedAccount?.active != false) "Disable" else "Enable",
-                        enabled = !pagerState.isScrollInProgress
-                    ) { vm.onEvent(ManageAccountsEvent.ToggleActive) }
-                    DashedCircularActionButton(
-                        icon = Icons.Default.AddCard,
-                        action = "Add Account",
-                        enabled = !pagerState.isScrollInProgress
-                    ) { }
-                }
+                AccountsPager(
+                    accounts = state.accounts,
+                    pagerState = pagerState,
+                    onAccountSelected = { vm.onEvent(ManageAccountsEvent.CardSelected(it)) }
+                )
+                AccountActions(
+                    enabled = !pagerState.isScrollInProgress,
+                    account = state.selectedAccount,
+                    onToggleActivate = { vm.onEvent(ManageAccountsEvent.ToggleActive) }
+                )
             }
         }
     }
 }
 
-@Composable
-private fun CircularActionButton(
-    modifier: Modifier = Modifier,
-    icon: ImageVector,
-    action: String,
-    enabled: Boolean = true,
-    onClick: () -> Unit
-) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier
-                .size(70.dp)
-                .background(
-                    MaterialTheme.colors.surface,
-                    RoundedCornerShape(100)
-                )
-                .clip(RoundedCornerShape(100))
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = if(enabled) LocalIndication.current else null
-                ) { if(enabled) onClick() },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                imageVector = icon,
-                contentDescription = icon.name
-            )
-        }
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(
-            text = action,
-            style = MaterialTheme.typography.button.copy(
-                fontWeight = FontWeight.Normal,
-                letterSpacing = 0.65.sp,
-                fontSize = 14.sp
-            ),
-            color = MaterialTheme.colors.onBackground
-        )
-    }
-}
-
-@Composable
-private fun DashedCircularActionButton(
-    modifier: Modifier = Modifier,
-    strokeColor: Color = MaterialTheme.colors.secondary,
-    icon: ImageVector,
-    action: String,
-    enabled: Boolean = true,
-    onClick: () -> Unit
-) {
-    val stroke = Stroke(
-        width = 2.5f,
-        pathEffect = PathEffect.dashPathEffect(floatArrayOf(12f, 12f), 0.1f)
-    )
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(
-            modifier = Modifier
-                .size(70.dp)
-                .drawBehind {
-                    drawRoundRect(
-                        color = strokeColor,
-                        style = stroke,
-                        cornerRadius = CornerRadius(40.dp.toPx())
-                    )
-                }
-                .clip(RoundedCornerShape(100))
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = if(enabled) LocalIndication.current else null
-                ) { if(enabled) onClick() },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                imageVector = icon,
-                contentDescription = icon.name
-            )
-        }
-        Spacer(modifier = Modifier.height(12.dp))
-        Text(
-            text = action,
-            style = MaterialTheme.typography.button.copy(
-                fontWeight = FontWeight.Normal,
-                letterSpacing = 0.65.sp,
-                fontSize = 14.sp
-            ),
-            color = MaterialTheme.colors.onBackground
-        )
-    }
-}
