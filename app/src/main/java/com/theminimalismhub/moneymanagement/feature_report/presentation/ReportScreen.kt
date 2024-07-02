77d096308e49f47907988fdd78365e12e17ecd91
Strahinja Sekulic <strahinja0123@gmail.com>
2024-07-02 14:21:07 +0200

    Add: Most expensive items

--- app/src/main/java/com/theminimalismhub/moneymanagement/feature_report/presentation/ReportScreen.kt
+++ app/src/main/java/com/theminimalismhub/moneymanagement/feature_report/presentation/ReportScreen.kt
@@ -1,28 +1,96 @@
 package com.theminimalismhub.moneymanagement.feature_report.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.theminimalismhub.moneymanagement.R
 import com.theminimalismhub.moneymanagement.core.composables.ScreenHeader
 import com.theminimalismhub.moneymanagement.core.transitions.BaseTransition
+import com.theminimalismhub.moneymanagement.core.utils.Currencier
+import com.theminimalismhub.moneymanagement.feature_finances.presentation.composables.CategoryTotalsOverview
+import com.theminimalismhub.moneymanagement.feature_finances.presentation.composables.FinanceCard
+import com.theminimalismhub.moneymanagement.feature_finances.presentation.home.HomeEvent
 
 @Destination(style = BaseTransition::class)
 @Composable
 fun ReportScreen(vm: ReportVM = hiltViewModel()) {
-    Column(
+
+    val state = vm.state.value
+
+    LazyColumn(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ScreenHeader(
-            title = stringResource(id = R.string.rs_title),
-            hint = stringResource(id = R.string.rs_hint)
-        )
+        item {
+
+            Text(
+                text = "${Currencier.formatAmount(state.total)} ${state.currency}",
+                style = MaterialTheme.typography.h1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 26.dp)
+                    .padding(top = 64.dp, bottom = 2.dp)
+            )
+            Text(
+                text = "Total spent in ${state.year}.",
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .alpha(0.65f)
+                    .padding(horizontal = 27.dp)
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            CategoryTotalsOverview(
+                totalPerCategory = state.totalPerCategory,
+                categoryBarStates = state.categoryBarStates,
+                currency = state.currency,
+                collapsable = true,
+                showCount = true
+            ) { }
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "10 Biggest Purchases",
+                style = MaterialTheme.typography.h2,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .alpha(0.65f)
+                    .padding(horizontal = 27.dp)
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+        }
+
+        items(state.results.take(10)) {
+            FinanceCard(
+                modifier = Modifier.padding(horizontal = 4.dp),
+                finance = it,
+                previousSegmentDate = state.results.getOrNull(state.results.indexOf(it) - 1)?.getDay(),
+                currency = state.currency,
+                showSeparator = false,
+                onEdit = { }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(24.dp))
+        }
     }
 }
--- app/src/main/java/com/theminimalismhub/moneymanagement/feature_report/presentation/ReportVM.kt
+++ app/src/main/java/com/theminimalismhub/moneymanagement/feature_report/presentation/ReportVM.kt
@@ -1,13 +1,85 @@
 package com.theminimalismhub.moneymanagement.feature_report.presentation
 
+import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.theminimalismhub.moneymanagement.core.enums.FinanceType
+import com.theminimalismhub.moneymanagement.feature_finances.domain.use_cases.HomeUseCases
+import com.theminimalismhub.moneymanagement.feature_finances.presentation.home.CategoryAmount
+import com.theminimalismhub.moneymanagement.feature_finances.presentation.home.CategoryBarState
+import com.theminimalismhub.moneymanagement.feature_finances.presentation.home.HomeState
+import com.theminimalismhub.moneymanagement.feature_settings.domain.Preferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import java.time.LocalDateTime
+import java.util.Calendar
 import javax.inject.Inject
 
 @HiltViewModel
 class ReportVM @Inject constructor(
+    private val useCases: HomeUseCases,
+    private val preferences: Preferences
 
 ) : ViewModel() {
 
+    private val _state = mutableStateOf(ReportState())
+    val state: State<ReportState> = _state
+
+    init {
+        _state.value = _state.value.copy(
+            limit = preferences.getSimpleLimit().toDouble(),
+            currency = preferences.getCurrency()
+        )
+        setRange(_state.value.year)
+        getFinances()
+    }
+
+    private fun setRange(year: Int) {
+        val start: Calendar = Calendar.getInstance()
+        start.set(year, 0, 1, 0, 0)
+        val end: Calendar = Calendar.getInstance()
+        if (year != LocalDateTime.now().year) {
+            end.set(year, 11, 31, 23, 59)
+        }
+        Log.i("DATE_RANGE", "${start.timeInMillis} - ${end.timeInMillis}")
+        _state.value = _state.value.copy(dateRange = Pair(start.timeInMillis, end.timeInMillis))
+    }
+
+    private var getFinancesJob: Job? = null
+    private fun getFinances() {
+        getFinancesJob?.cancel()
+        getFinancesJob = useCases.getFinances(_state.value.dateRange, categoryId = null, accountId = null, mutableListOf(FinanceType.OUTCOME), mutableListOf(true, false))
+            .onEach { finance ->
+                _state.value = _state.value.copy(
+                    results = finance.sortedBy { it.finance.amount }.reversed(),
+                    total = finance.sumOf { it.finance.amount }
+                )
+                getCategoryTotals()
+            }
+            .launchIn(viewModelScope)
+    }
+    private fun getCategoryTotals() {
+        val bars: MutableList<CategoryAmount> = mutableListOf()
+        _state.value.results.filter{ it.finance.type == FinanceType.OUTCOME }.groupBy { it.category!!.categoryId }.forEach {
+            bars.add(
+                CategoryAmount(
+                    categoryId = it.key!!,
+                    accountId = it.value[0].account.accountId!!,
+                    name = it.value[0].category!!.name,
+                    color = it.value[0].category!!.color,
+                    amount = it.value.sumOf { it.finance.amount },
+                    count = it.value.count()
+                )
+            )
+        }
+        _state.value = _state.value.copy(totalPerCategory = bars.sortedBy { it.amount }.reversed())
+        bars.forEach {
+            _state.value.categoryBarStates[it.categoryId] = mutableStateOf(CategoryBarState.NEUTRAL)
+        }
+    }
 
 }
