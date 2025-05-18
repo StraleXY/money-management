98668e678676befcc3e9d0d5ba67e472fbf74a6b
Strahinja Sekulic <strahinja0123@gmail.com>
2025-05-18 18:31:51 +0200

    Add: WipeReveal text

--- app/src/main/java/com/theminimalismhub/moneymanagement/feature_funds/presentation/manage_funds/presentation/FundSelectorPager.kt
+++ app/src/main/java/com/theminimalismhub/moneymanagement/feature_funds/presentation/manage_funds/presentation/FundSelectorPager.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.scale
@@ -25,8 +26,10 @@ import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
@@ -37,6 +40,7 @@ import com.theminimalismhub.moneymanagement.core.enums.FundType
 import com.theminimalismhub.moneymanagement.feature_funds.data.model.FundItem
 import com.theminimalismhub.moneymanagement.feature_funds.domain.model.Fund
 import com.theminimalismhub.moneymanagement.feature_funds.presentation.manage_funds.presentation.FundCards.DisplayFundCard
+import kotlin.math.abs
 import kotlin.math.absoluteValue
 import kotlin.math.max
 
@@ -60,11 +64,8 @@ fun FundSelectorPager(
         "Savings"
     )
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-
+    Column(modifier = Modifier.fillMaxWidth()) {
+        
         Row(
             modifier = Modifier
                 .padding(start = 24.dp)
@@ -128,66 +129,39 @@ fun FundSelectorPager(
 @OptIn(ExperimentalPagerApi::class)
 @Composable
 fun WipeRevealText(
-    width: Dp = 150.dp,
+    width: Dp = 180.dp,
     messages: List<String>,
-    intervalMillis: Long = 1000,
     pagerState: PagerState
 ) {
-    var currentIndex by remember { mutableStateOf(0) }
-    var animationProgress by remember { mutableStateOf(1f) } // 1 = fully revealed
 
-    val nextIndex = (pagerState.currentPage + 1) % messages.size
-
-    // Start a looping effect
-//    LaunchedEffect(currentIndex) {
-//        while (true) {
-//            delay(intervalMillis)
-//
-//            // Animate wipe (0f to 1f)
-//            val duration = 3000
-//            animate(
-//                initialValue = 0f,
-//                targetValue = 1f,
-//                animationSpec = tween(durationMillis = duration)
-//            ) { value, _ ->
-//                animationProgress = value
-//            }
-//
-//            // Once done, switch text
-//            currentIndex = nextIndex
-//            animationProgress = 0f
-//        }
-//    }
-
-    
-
+    val nextIndex = pagerState.targetPage ?: ((pagerState.currentPage + 1) % messages.size)
 
     Box(
         modifier = Modifier
-            .height(32.dp)
+            .height(36.dp)
             .clipToBounds()
     ) {
         // Bottom text (next one)
         Text(
-            text = messages[nextIndex],
-            style = MaterialTheme.typography.h3,
+            text = messages.getOrNull(nextIndex) ?: "Unknown",
+            style = MaterialTheme.typography.h4.copy(fontSize = 26.sp, fontWeight = FontWeight.Medium),
             modifier = Modifier
                 .width(width)
                 .align(Alignment.Center)
                 .padding(
                     end = 0.dp,
-                    start = max(24 * (2 - pagerState.currentPageOffset), 0f).dp
+                    start = max(24 * (if(pagerState.currentPageOffset >= 0) 2 - abs(pagerState.currentPageOffset) else abs(pagerState.currentPageOffset)), 0f).dp
                 )
                 .graphicsLayer {
                     clip = true
                     shape = RectangleShape
                 }
                 .drawWithContent {
-                    val wipeWidth = size.width * (1 - pagerState.currentPageOffset)
+                    val wipeWidth = size.width * (1 - abs(pagerState.currentPageOffset))
                     clipRect(
-                        left = wipeWidth,
+                        left = if(pagerState.currentPageOffset >= 0) wipeWidth else 0f ,
+                        right = if(pagerState.currentPageOffset >= 0) size.width else size.width * abs(pagerState.currentPageOffset),
                         top = 0f,
-                        right = size.width,
                         bottom = size.height
                     ) {
                         this@drawWithContent.drawContent()
@@ -197,25 +171,26 @@ fun WipeRevealText(
         )
         // Top text with wipe animation (current one being revealed away)
         Text(
-            text = messages[pagerState.currentPage],
-            style = MaterialTheme.typography.h3,
+            text = messages.getOrNull(pagerState.currentPage) ?: "Unknown",
+            style = MaterialTheme.typography.h4.copy(fontSize = 26.sp, fontWeight = FontWeight.Medium),
             modifier = Modifier
                 .width(width)
                 .align(Alignment.Center)
                 .padding(
-                    end = max(24 * pagerState.currentPageOffset, 0f).dp,
-                    start = max(24 * (1 - pagerState.currentPageOffset), 0f).dp
+                    end = max(24 * abs(pagerState.currentPageOffset), 0f).dp,
+                    start = max(24 * (if(pagerState.currentPageOffset >= 0) 1 - abs(pagerState.currentPageOffset) else 1 + abs(pagerState.currentPageOffset)), 0f).dp
                 )
+                .alpha(if(pagerState.currentPageOffset == 0f) 1f else 1 - abs(pagerState.currentPageOffset))
                 .graphicsLayer {
                     clip = true
                     shape = RectangleShape
                 }
                 .drawWithContent {
-                    val wipeWidth = size.width * (1 - pagerState.currentPageOffset)
+                    val wipeWidth = size.width * (1 - abs(pagerState.currentPageOffset))
                     clipRect(
-                        left = 0f,
+                        left = if(pagerState.currentPageOffset >= 0) 0f else size.width,
+                        right = if(pagerState.currentPageOffset >= 0) wipeWidth else size.width - wipeWidth,
                         top = 0f,
-                        right = wipeWidth,
                         bottom = size.height
                     ) {
                         this@drawWithContent.drawContent()
